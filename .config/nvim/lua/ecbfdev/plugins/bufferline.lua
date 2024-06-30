c8125336edf09aa6403e50bddf9d677c0af5f404
Edwin Carl Flores <edwincarlflores@gmail.com>
2024-06-30 20:29:21 +1200

    Update lazy lock

--- .config/nvim/lua/ecbfdev/plugins/bufferline.lua
+++ .config/nvim/lua/ecbfdev/plugins/bufferline.lua
@@ -2,15 +2,16 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
+  enabled = false,
   opts = {
     options = {
       offsets = {
         {
-            filetype = "NvimTree",
-            text="",
-            separator= true,
-            text_align = "left"
-        }
+          filetype = "NvimTree",
+          text = "",
+          separator = true,
+          text_align = "left",
+        },
       },
       -- mode = "tabs",
       -- separator_style = "slant",
